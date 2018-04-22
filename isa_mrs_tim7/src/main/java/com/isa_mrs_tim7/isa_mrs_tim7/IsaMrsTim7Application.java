89f864d95b8c4e9b48d3332cb819272682985170
Marko Mucalov <mucalovmarko AT gmail DOT com>
2018-04-23 01:14:33 +0200

    inicijalni komit (prazan maven projekat)

---
+++ isa_mrs_tim7/src/main/java/com/isa_mrs_tim7/isa_mrs_tim7/IsaMrsTim7Application.java
@@ -0,0 +1,12 @@
+package com.isa_mrs_tim7.isa_mrs_tim7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaMrsTim7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaMrsTim7Application.class, args);
+	}
+}
---
+++ isa_mrs_tim7/src/test/java/com/isa_mrs_tim7/isa_mrs_tim7/IsaMrsTim7ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isa_mrs_tim7.isa_mrs_tim7;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsaMrsTim7ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
