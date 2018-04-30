4f00d925f6b870043aea630cabb1de53998c6fce
Marko Mucalov <mucalovmarko AT gmail DOT com>
2018-04-30 16:59:50 +0200

    Prepravljena aplikacija u single page, dodate Google mape.

--- isa_mrs_tim7/src/main/resources/public/pozorista.js
+++ isa_mrs_tim7/src/main/resources/public/pozorista.js
@@ -1,54 +1,68 @@
 function dobaviPozorista() {
-    /*var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:8080/pozorista", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var resp = xhttp.responseText;
-    var response = JSON.parse(xhttp.responseText);*/
-	$.getJSON('http://localhost:8080/pozorista', function (data) {
+	
+	$.get('http://localhost:8080/pozorista', function (data) {
 		var response = data;
-		var urlConcat = "";
-	    for (var counter in response.content) {
-	        urlConcat = urlConcat+response.content[counter].naziv+",";
-			urlConcat = urlConcat+response.content[counter].rejting+",";
-	        var adresaTemp = response.content[counter].adresa;
-	        urlConcat = urlConcat+adresaTemp.ulica+",";
-	        urlConcat = urlConcat+adresaTemp.broj+",";
-	        urlConcat = urlConcat+adresaTemp.grad+",";
-	        urlConcat = urlConcat+adresaTemp.zip+";";
-	    }
-	    window.location.href='/pozorista.html'+'#'+urlConcat;
-})
-    
+		$("#pocetna").hide();
+		$("#pozorista_prikaz").show();
+		var table = document.getElementById("tabela");
+		var countRow = 0;
+		
+		for(var counter in response.content){
+			var row = table.insertRow(countRow);
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+			var cell5 = row.insertCell(4);
+			var adresaTemp = response.content[counter].adresa;
+			cell1.innerHTML = countRow+1;
+			cell2.innerHTML = response.content[counter].naziv;
+			cell3.innerHTML = adresaTemp.ulica+" "+adresaTemp.broj+" "+adresaTemp.grad+" "+adresaTemp.zip;
+			cell4.innerHTML = "udaljenost";
+			cell5.innerHTML = '<button class="btn btn-primary">repertoar</button>';	
+			countRow++;
+		}
+	})
+     getMapLocation();
 }
 
-function parsirajPozorista(){
-	var trenutniUrl = window.location.href;
-	trenutniUrl = trenutniUrl.split("#")[1];
-	var pozorista = trenutniUrl.split(';');
-	var brojac = 0;
+function povratakNaPocetnu(){
+	$("#pocetna").toggle();
+	$("#pozorista_prikaz").toggle();
+	$("#tabela").empty();
 	
-	for(var poz in pozorista){
-		brojac++;
-		var delovi = pozorista[poz].split(',');
-		var naziv = delovi[0];
-		var rejting = delovi[1];
-		var ulica = delovi[2];
-		var broj = delovi[3];
-		var grad = delovi[4];
-		var zip = delovi[5];
-		//$('#tabela').after('<tr><td>'+document.write(naziv)+'</td>'+'<td>'+document.write(rejting)+'</td>'+'<td>'+document.write(ulica)+' '+document.write(broj)+' '+document.write(zip)+' '+document.write(grad)+'</td></tr>');
-		var table = document.getElementById("tabela");
-		var row = table.insertRow(brojac-1);
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
-		var cell4 = row.insertCell(3);
-		var cell5 = row.insertCell(4);
-		cell1.innerHTML = brojac;
-		cell2.innerHTML = naziv;
-		cell3.innerHTML = ulica+" "+broj+" "+zip+" "+grad;
-		cell4.innerHTML = "udaljenost";
-		cell5.innerHTML = "repertoar";
-	}
 }
+
+var y = document.getElementById("map");
+var mapLatitude;
+var mapLongitude;
+var myLatlng;
+var map;
+
+function getMapLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showMapPosition);
+    } else {
+        y.innerHTML = "Lokacija nije podrzana za ovaj pretrazivac.";
+    }
+}
+function showMapPosition(position) {
+    mapLatitude = position.coords.latitude;
+    mapLongitude = position.coords.longitude;
+    myLatlng = new google.maps.LatLng(mapLatitude,mapLongitude);
+    initMap();
+}
+
+function initMap() {
+	
+    var uluru = {lat: 44.787197, lng: 20.457273};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 6,
+      center: new google.maps.LatLng(mapLatitude, mapLongitude)
+    });
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title:"Vasa pozicija"
+    });
+  }
